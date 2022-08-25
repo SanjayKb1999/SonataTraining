2a3e192cfae76ad3e307a14cafdea0278dd94bf5
Sanjay <sanjay.kb@sonata-software.com>
2022-08-25 11:42:40 +0530

    done

--- src/test/java/com/sonata/Test/CalTest.java
+++ src/test/java/com/sonata/Test/CalTest.java
@@ -1,11 +1,13 @@
 package com.sonata.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
+
 import com.sonata.Calculator.Calculator;
 
 
-public class CalTest {
-	
+ class CalTest {
+	@Test
 	    public void divTest() {
 	    	Calculator c1 = new Calculator();
 	    	assertThrows(ArithmeticException.class,()->c1.div(10,0),"it is an error");
