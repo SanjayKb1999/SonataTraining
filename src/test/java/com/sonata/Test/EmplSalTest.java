9abdc8be54eb0c8e43ce09d63aba7e6f69a03f07
Sanjay <sanjay.kb@sonata-software.com>
2022-08-25 11:37:53 +0530

    done

--- src/test/java/com/sonata/Test/EmplSalTest.java
+++ src/test/java/com/sonata/Test/EmplSalTest.java
@@ -1,10 +1,12 @@
 package com.sonata.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 import com.sonata.Impl.EmpImpl;
 import com.sonata.Model.Employee;
 public class EmplSalTest{
-//@Test
+@Test
 public void addTest(){
 	Employee es= new Employee();
 	EmpImpl e= new EmpImpl();
